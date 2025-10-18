c1b9c5ab6c20ef221a5db33b7d33fee4714aabfe
Gildman Laércio <laerciokarlagabm@gmail.com>
2025-10-18 06:37:46 -0300

    menu-toggle and index.js fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,20 @@
+const content = document.getElementById('content');
 const menuToggle = document.getElementById('menu-toggle');
-// const navMenu = document.querySelector(".header__nav");
 const navMenu = document.getElementById('site-nav');
 const menuIcon = document.getElementById('menu-icon');
 
-// let isOpen = false;
 
 menuToggle.addEventListener('click', () => {
   const isOpen = navMenu.classList.toggle('is-open');
-  // navMenu.classList.toggle('show');
   menuToggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
 
-  // isOpen = !isOpen;
   if (isOpen) {
     menuIcon.src = 'assets/images/icon-menu-close.svg';
     menuIcon.alt = 'Close menu';
+    content.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
     menuIcon.src = 'assets/images/icon-menu.svg';
     menuIcon.alt = 'Open menu';
+    content.style.backgroundColor = 'rgb(255,255,255)';
   }
 })
